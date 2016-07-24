72f578bcea4d83ff708a3027beb63074c488a9e9
tadakazu <tadakazu1972@gmail.com>
2016-07-24 17:32:49 +0900

    change KinentaiActivity.

--- app/src/main/java/tadakazu1972/fireemergency/KinentaiActivity.java
+++ app/src/main/java/tadakazu1972/fireemergency/KinentaiActivity.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by tadakazu on 2016/07/12.
@@ -50,6 +51,12 @@ public class KinentaiActivity extends AppCompatActivity {
                startActivity(intent);
            }
         });
+        mView.findViewById(R.id.btnKinentai1).setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                showKinentai1();
+            }
+        });
         mView.findViewById(R.id.btnKinentaiEarthquake).setOnClickListener(new OnClickListener(){
             @Override
             public void onClick(View v){
@@ -80,6 +87,60 @@ public class KinentaiActivity extends AppCompatActivity {
                 showWeather();
             }
         });
+    }
+
+    //震央「陸」
+    private void showKinentai1(){
+        final CharSequence[] actions = {"■震度７(特別区６強)","■震度６強(特別区６弱)","■震度６弱(特別区５強、政令市５強又は６弱)"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("震度は？");
+        builder.setItems(actions, new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                switch(which){
+                    case 0:
+                        showRiku7();
+                        break;
+                    case 1:
+                        showRiku7();
+                        break;
+                    case 2:
+                        showRiku7();
+                        break;
+                }
+            }
+        });
+        builder.setNegativeButton("キャンセル", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                //何もしない
+            }
+        });
+        builder.setCancelable(true);
+        builder.create();
+        builder.show();
+    }
+
+    //震度７
+    private void showRiku7(){
+        final CharSequence[] actions = {"北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","山梨県","新潟県","富山県","石川県","福井県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("■震度７(特別区６強)の都道府県は？");
+        builder.setItems(actions, new DialogInterface.OnClickListener(){
+           @Override
+            public void onClick(DialogInterface dialog, int which){
+               Toast.makeText(mActivity, String.valueOf(which)+"番目が選択されました", Toast.LENGTH_LONG).show();
+           }
+        });
+        builder.setNegativeButton("キャンセル", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                //何もしない
+            }
+        });
+        builder.setCancelable(true);
+        builder.create();
+        builder.show();
     }
 
     //情報（地震）
